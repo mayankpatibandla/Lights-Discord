9f14bdd84e944209c67193bb43e2159f107d8807
Mayank Patibandla <34776435+mayankpatibandla@users.noreply.github.com>
2023-09-28 23:37:12 -0400

    Fix message max length?

--- bot.py
+++ bot.py
@@ -200,8 +200,8 @@ async def slash_command_list(interaction: Interaction):
     description="Prints the current pattern",
 )
 async def slash_command_print(interaction: Interaction):
-    print_str = ', '.join([f'`{i}: {format_color(x)}`' for i, x in enumerate(lc.lights[:])])
-    embed = nextcord.Embed(title="Current Pattern", color=0x00FF00)
+    print_str = ', '.join([f'`{i} {format_color(x)}`' for i, x in enumerate(lc.lights[:])])
+    embed = nextcord.Embed(title="Current Pattern", color=0x00FF00, description=print_str)
     await interaction.response.send_message(f"Current pattern: {len(print_str)}", embed=embed)
 
 
